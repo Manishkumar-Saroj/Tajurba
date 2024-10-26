527489a6d57ff697bcc7ba017b449e3a6f02d7d2
ManishSaroj <manishsaroj5259@gmail.com>
2024-10-26 08:24:04 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        grid: "grid 15s linear infinite",
+      },
+      keyframes: {
+        grid: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      screens: {
+        'max-450': {'max': '450px'},
+      }
+    },
   },
   plugins: [],
 }
